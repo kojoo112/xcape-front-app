55299f8b1eb6cd685a5869955bac54d36b08643f
kojoo112 <kapella000@gmail.com>
2024-08-23 01:20:23 +0900

    remove 게임 text

--- src/components/Information.tsx
+++ src/components/Information.tsx
@@ -30,15 +30,19 @@ function Information() {
                             </div>
                             <div className="mx-0 my-auto">
                                 <div className="text-3xl text-white">엑스파일러게임</div>
-                                <div className="text-xl text-[#ff7119]">사건수사 프로파일링 게임</div>
+                                <div className="text-xl text-[#ff7119]">사건수사 프로파일링</div>
                             </div>
                         </div>
                         <div className="grid grid-cols-2 gap-2 px-2">
-                            {xfilerThemeList.sort((prev, next) => {
-                              return next.id! - prev.id!;
-                            }).map((theme, index) => {
-                                return <ThemeInfo key={theme.id} theme={theme} index={index} color={theme.colorCode} />;
-                            })}
+                            {xfilerThemeList
+                                .sort((prev, next) => {
+                                    return next.id! - prev.id!;
+                                })
+                                .map((theme, index) => {
+                                    return (
+                                        <ThemeInfo key={theme.id} theme={theme} index={index} color={theme.colorCode} />
+                                    );
+                                })}
                         </div>
                     </div>
                 )}
@@ -51,15 +55,17 @@ function Information() {
                             </div>
                             <div className="mx-0 my-auto">
                                 <div className="text-3xl text-white">크라임씬게임</div>
-                                <div className="text-xl text-[#fff200]">용의자 역할극 롤플레잉 게임</div>
+                                <div className="text-xl text-[#fff200]">용의자 역할극 롤플레잉</div>
                             </div>
                         </div>
                         <div className="grid grid-cols-2 gap-2 px-2">
-                            {crimeSceneThemeList.sort((prev, next) => {
-                              return next.id! - prev.id!;
-                            }).map((theme, index) => {
-                                return <ThemeInfo key={theme.id} theme={theme} index={index} color="#fff200" />;
-                            })}
+                            {crimeSceneThemeList
+                                .sort((prev, next) => {
+                                    return next.id! - prev.id!;
+                                })
+                                .map((theme, index) => {
+                                    return <ThemeInfo key={theme.id} theme={theme} index={index} color="#fff200" />;
+                                })}
                         </div>
                     </>
                 )}
